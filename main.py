91071ab61203da0bc9ff356c637ec47dd7ebf4ac
MyLongCode <48795877+MyLongCode@users.noreply.github.com>
2021-12-30 02:01:47 +0500

    Cделал красивое меню и генерацию случайных слов в игре Крокодил.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import QEvent
 from PyQt5.QtWidgets import *
-import paint
+import crocodile
+
+
 
 
 class Window(QMainWindow):
@@ -9,47 +12,119 @@ class Window(QMainWindow):
         self.name = name
         self.setMinimumSize(QtCore.QSize(1000, 800))
         self.setWindowTitle("Главное меню")
-        fontButton = QtGui.QFont()  # Cтили текста для кнопок и лейблов
+        self.setStyleSheet("background-color:#3771A1; color:white;border:none;")
+        self.stop = False #Для работы кнопки
+
+
+
+        frameTop = QFrame(self) #фрейм с надписью MiniGames
+        frameTop.setFrameShape(QFrame.StyledPanel)
+        frameTop.resize(1000,200)
+        frameTop.move(0,0)
+        frameTop.setStyleSheet("background-color:#3771A1;border:none;")
+
+
+        frameBorder = QFrame(self) # горизонтальная палка
+        frameBorder.setFrameShape(QFrame.StyledPanel)
+        frameBorder.resize(1000,10)
+        frameBorder.move(0,100)
+        frameBorder.setStyleSheet("background-color:#000000;border:none;")
+
+        frameBorder2 = QFrame(self)  # вертикальная палка
+        frameBorder2.setFrameShape(QFrame.StyledPanel)
+        frameBorder2.resize(10, 700)
+        frameBorder2.move(250, 102)
+        frameBorder2.setStyleSheet("background-color:#000000; border:1px solid #21222F;")
+
+
+        frameMain = QFrame(self)
+        frameMain.setFrameShape(QFrame.StyledPanel)
+        frameMain.resize(700, 500)
+        frameMain.setStyleSheet("background-color:#21222F;border:none;border-radius:15px;")
+        frameMain.move(280,120)
+
+
+
+        minigames = QLabel("MiniGames", self)
+        minigames.resize(500,80)
+        minigames.move(300,10)
+        fontMinigames = QtGui.QFont()
+        fontMinigames.setPointSize(60)
+        minigames.setFont(fontMinigames)
+        minigames.setStyleSheet("background-color:#3771A1;color:#FFFFFF;font-weight:bold;")
+
+
+
+        fontButton = QtGui.QFont()
         fontButton.setPointSize(24)
-        fontLabel1 = QtGui.QFont()
-        fontLabel1.setPointSize(30)
+        self.button1 = QPushButton('Крокодил', self)
+        self.button1.setStyleSheet("background-color:#21222F;border:none;border-radius:15px;")
+        self.button1.resize(200, 50)
+        self.button1.move(25, 130)
+        self.button1.setFont(fontButton)
+        self.button1.clicked.connect(self.push)
+
+
+
+
         fontLabel2 = QtGui.QFont()
         fontLabel2.setPointSize(34)
+        label2 = QLabel("   Ваш ник: " + self.name, self)
+        label2.resize(600, 100)
+        label2.setFont(fontLabel2)
+        label2.setStyleSheet("background-color:#21222F;border:none;border-radius:15px;")
+        label2.move(280, 650)
+
+        self.info = "Новости: Патч v0.001. добавлен интерфейс и возможность вводить ник."
         fontLabel3 = QtGui.QFont()
         fontLabel3.setPointSize(20)
-        button1 = QPushButton('Крокодил', self)
-        button1.resize(250, 50)
-        button1.move(25, 115)
-        button1.setFont(fontButton)
-        button1.clicked.connect(self.push)
-        label1 = QLabel("Список игр:", self)
-        label1.resize(250, 50)
-        label1.setFont(fontLabel1)
-        label1.move(25, 50)
-        label2 = QLabel("Ваш ник: " + self.name, self)
-        label2.resize(500, 50)
-        label2.setFont(fontLabel2)
-        label2.move(380, 350)
-        label3 = QLabel("Новости: Патч v0.001 добавлен интерфейс и возможность вводить ник", self)
-        label3.resize(400, 400)
-        label3.move(450, -100)
-        label3.setFont(fontLabel3)
-        label3.setWordWrap(True)
+        self.label3 = QLabel(self.info, self)
+        self.label3.resize(605, 400)
+        self.label3.setStyleSheet("background:#21222F;")
+        self.label3.move(300, 130)
+        self.label3.setFont(fontLabel3)
+        self.label3.setWordWrap(True)
+
+        fontButton = QtGui.QFont()
+        fontButton.setPointSize(24)
+        self.button2 = QPushButton('Играть', self)
+        self.button2.setStyleSheet("background-color:#3771A1;border:none;border-radius:15px;")
+        self.button2.resize(250, 50)
+        self.button2.move(500, 550)
+        self.button2.setFont(fontButton)
+        self.button2.clicked.connect(self.go)
+        self.button2.hide()
+
+
 
     def push(self):
-        ex.show()  # Переход в файл paint.py
+        if self.button1.text() == "Назад":
+            self.button1.setText("Крокодил")
+            self.label3.setText("Новости: Патч v0.001. добавлен интерфейс и возможность вводить ник.")
+            self.button2.hide()
+
+        elif self.button1.text() == "Крокодил":
+            self.label3.setText("Правила этой игры очень просты и будут понятны даже ребенку. Один из игроков получает случайное слово или словосочетание"
+                                ", и он должен нарисовать его. "
+                                "Другие игроки должно догадаться, что за слово загадано и написать его в чат")
+            self.button2.show()
+            self.button1.setText("Назад")
+
+    def go(self):
+        CROCODILE.show()  # Переход в файл crocodile.py
 
 
 if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
-    d, e = QInputDialog.getText(None, 'Login', 'Введите ник:', QLineEdit.Normal)
-    if not d.split():
+    nickname, e = QInputDialog.getText(None, 'Вход в систему', 'Введите ник:', QLineEdit.Normal)
+    if not nickname.split():
         e = False
     if not e:
         sys.exit(app.exec_())
-    ex = paint.Window()
-    w = Window(d)
-    w.show()
+    nickname = "MyLongCode"
+    CROCODILE = crocodile.Window() #крокодил
+    win = Window(nickname) #главное меню
+    win.show()
     sys.exit(app.exec_())
--- test.py
+++ test.py
@@ -1,8 +1,82 @@
-import socket
+import sys
+from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtCore import QPropertyAnimation, pyqtProperty
+from PyQt5.QtWidgets import QGraphicsDropShadowEffect, QApplication, QMainWindow
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создаем сокет
-sock.connect(('89.232.193.250', 25565))  # подключемся к серверному сокету
-sock.send(bytes('Hello, world', encoding = 'UTF-8'))  # отправляем сообщение
-data = sock.recv(1024)  # читаем ответ от серверного сокета
-sock.close()  # закрываем соединение
-print(data)
+
+class PushButton(QtWidgets.QPushButton):
+    hover = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(PushButton, self).__init__(parent)
+        pass
+
+    def enterEvent(self, event):
+        self.hover.emit("enterEvent")
+
+    def leaveEvent(self, event):
+        self.hover.emit("leaveEvent")
+
+
+class AnimationShadowEffect(QGraphicsDropShadowEffect):
+
+    def __init__(self, color, *args, **kwargs):
+        super(AnimationShadowEffect, self).__init__(*args, **kwargs)
+        self.setColor(color)
+        self.setOffset(0, 0)
+        self.setBlurRadius(0)
+        self._radius = 0
+        self.animation = QPropertyAnimation(self)
+        self.animation.setTargetObject(self)
+        self.animation.setDuration(1500)            # Время одного цикла
+        self.animation.setLoopCount(-1)             # Постоянный цикл
+        self.animation.setPropertyName(b'radius')
+        self.animation.setKeyValueAt(0.5, 15)
+
+    def start(self):
+        self.animation.start()
+
+    def stop(self, r=0):
+        # Остановить анимацию и изменить значение радиуса
+        self.animation.stop()
+        self.radius = r
+
+    @pyqtProperty(int)
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, r):
+        self._radius = r
+        self.setBlurRadius(r)
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        uic.loadUi("main.ui", self)
+
+        #Инифицализация анимации
+        self.aniButton = AnimationShadowEffect(QtCore.Qt.white, self.designer_button)
+        self.designer_button.setGraphicsEffect(self.aniButton)
+
+        #Добавление эффекта к кнопке
+        self.designer_button = PushButton(self.designer_button)
+        self.designer_button.setCheckable(True)
+        self.designer_button.hover.connect(self.button_hover)
+
+
+    def button_hover(self, hover):
+        if hover == "enterEvent":
+            self.aniButton.start()
+        elif hover == "leaveEvent":
+            self.aniButton.stop()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    ex = App()
+    ex.show()
+    sys.exit(app.exec_())
